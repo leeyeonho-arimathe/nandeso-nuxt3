7cf58071d0c71dc0563af9d74a9ca84fc3cc5c95
Yeonho Lee <mclljr123@gmail.com>
2022-07-17 22:13:21 +0900

    - change header navigation

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar fixed-top navbar-expand-md navbar-light bg-light">
+  <nav class="navbar fixed-top navbar-expand-md navbar-light bg-light header-container">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Nandeso</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,17 +7,32 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ms-auto">
-          <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Search</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Support</a></li>
+          <li class="nav-item"><NuxtLink to="/" class="nav-link active">Home</NuxtLink></li>
+          <li class="nav-item"><NuxtLink to="/support" class="nav-link">Support</NuxtLink></li>
         </ul>
         <div>
-          <button class="btn btn-primary" type="button">Signup</button> <button class="btn btn-secondary" type="button">Member</button>
+          <NuxtLink to="/sign-up" class="btn btn-primary">SignUp</NuxtLink> <NuxtLink to="/members" class="btn btn-primary">Members</NuxtLink>
         </div>
       </div>
     </div>
   </nav>
 </template>
 
+<script>
+export default {
+  head: {
+    script: [
+      {
+        defer: true,
+        type: 'module',
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js'
+      }
+    ]
+  }
+}
+</script>
+
 <style scoped>
 
 </style>
+
