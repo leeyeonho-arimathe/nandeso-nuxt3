54e42476cb5d5b7aa5c6d7741b6635da97063e38
Yeonho Lee <mclljr123@gmail.com>
2022-07-17 10:40:46 +0900

    - add main layout and search.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <div>
-      Nandeso
-    </div>
+  <div class="container">
+    <HeaderNav />
+  </div>
+  <div class="container">
+    <Search />
   </div>
 </template>
+
+<script>
+export default {
+  head: {
+    script: [
+      {
+        type: 'module',
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js'
+      }
+    ]
+  }
+}
+</script>
---
+++ components/Search.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mt-5 justify-content-center">
+    <div class="form-search">
+      <form>
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Keyword" aria-label="text with button addon" aria-describedby="button-addon1">
+          <button class="btn btn-outline-secondary" type="button" id="button-addon1">検索</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.form-search {
+  max-width: 720px;
+  padding: 15px;
+  margin: auto;
+}
+
+</style>
