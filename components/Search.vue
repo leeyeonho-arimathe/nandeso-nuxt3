234d66cf0736068d7ce938790aea4d9fc8178b92
Yeonho Lee <mclljr123@gmail.com>
2022-07-17 22:14:08 +0900

    - change search form

--- components/Search.vue
+++ components/Search.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="mt-5 justify-content-center">
+  <div class="mt-5 pt-5 justify-content-center search-container">
     <div class="form-search">
-      <!-- rotate typography -->
-      <p class="text-center">あなたが悩んでいるのは何ですか？少しお聞かせください。</p>
+      <p class="text-center fw-light">あなたが悩んでいるのは何ですか？少しお聞かせください。</p>
       <form>
         <div class="input-group">
           <input type="text" class="form-control" placeholder="Keyword" aria-label="text with button addon" aria-describedby="button-addon1">
