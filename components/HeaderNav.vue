a3dcca1170fdba6b6133ea300e838215d7af11dc
Yeonho Lee <mclljr123@gmail.com>
2022-07-17 10:22:34 +0900

    - add header container

---
+++ components/HeaderNav.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navbar fixed-top navbar-expand-md navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Nandeso</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Search</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Support</a></li>
+        </ul>
+        <div>
+          <button class="btn btn-primary" type="button">Signup</button> <button class="btn btn-secondary" type="button">Member</button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
