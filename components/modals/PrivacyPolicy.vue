9637b7ea661851d5569a6e7cbc69a90228be6ef2
Yeonho Lee <mclljr123@gmail.com>
2022-07-19 00:53:50 +0900

    - change privacy policy eng to jpn, remove app.vue

--- components/modals/PrivacyPolicy.vue
+++ components/modals/PrivacyPolicy.vue
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="privacy-policy-modalLabel">Privacy Policy</h5>
+          <h5 class="modal-title" id="privacy-policy-modalLabel">プライバシーポリシー</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
