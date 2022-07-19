4f54c60b91dfea6b640b6e7c19a2278ef3bbf7eb
Yeonho Lee <mclljr123@gmail.com>
2022-07-19 09:21:02 +0900

    - add google materal icons, write layout

--- pages/write.vue
+++ pages/write.vue
@@ -5,13 +5,26 @@
     </div>
     <div class="container mt-5 pt-3">
       <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        <label for="exampleFormControlInput1" class="form-label">タイトル</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1">
       </div>
       <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <label for="exampleFormControlTextarea1" class="form-label">相談内容</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlInput2" class="form-label">ハッシュタグ<a href="#"><span class="material-symbols-outlined">help</span></a></label>
+        <input type="text" class="form-control" id="exampleFormControlInput2">
+      </div>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <button class="btn btn-secondary" type="button">下書き</button>
+        <button class="btn btn-primary" type="button">公開</button>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+
+
+</style>
