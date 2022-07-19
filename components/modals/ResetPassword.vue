c49be18fd0b6bd65c4ccbea3da88bc45e0114cfc
Yeonho Lee <mclljr123@gmail.com>
2022-07-20 00:16:19 +0900

    - change login layout, reset password

---
+++ components/modals/ResetPassword.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="modal fade" id="reset-password-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="reset-password-modalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="reset-password-modalLabel">パスワードリセット</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div>
+          <section>
+            <h1></h1>
+            <p>30分以内にメール記載のリンクからパスワードを再設定してください。</p>
+          </section>
+          <div>
+            <div class="col-md-12">
+              <label for="validationServer01" class="form-label">メールアドレス</label>
+              <input type="text" class="form-control is-valid" id="validationServer01" placeholder="your_mail@domain.com" required>
+            </div>
+            <div class="mt-5">
+              <p>メールが届かない場合、以下をご確認ください。</p>
+              <ul>
+                <li>入力されたメールアドレスが正しいか</li>
+                <li>mail.nandeso.comのメールアドレスが拒否設定に含まれていないか</li>
+                <li>迷惑メールフォルダに入っていないか</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">クローズ</button>
+        <button type="button" class="btn btn-primary">完了する</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</template>
--- pages/login.vue
+++ pages/login.vue
@@ -6,20 +6,23 @@
     <div class="container mt-5 pt-3">
       <form>
         <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <label for="exampleInputEmail1" class="form-label">メールアドレス</label>
           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <label for="exampleInputPassword1" class="form-label">パスワード</label>
           <input type="password" class="form-control" id="exampleInputPassword1">
         </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <div class="mb-3 text-center">
+          <a href="#" data-bs-toggle="modal" data-bs-target="#reset-password-modal">パスワードが分からない方はこちら</a>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+          <button type="button" class="btn btn-primary">ログイン</button>
+        </div>
       </form>
     </div>
+    <!-- model -->
+    <ModalsResetPassword />
+    <!-- /model -->
   </div>
 </template>
